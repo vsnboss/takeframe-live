9fd088c9b272928e8224614a072b5a47cc4e5ae5
vsnboss <297455067+vsnboss@users.noreply.github.com>
2026-08-18 15:34:01 +0200

    Use current Revolut subscription API version

--- site/api/subscribe.js
+++ site/api/subscribe.js
@@ -1,6 +1,6 @@
 const crypto = require('crypto');
 
-const API_VERSION = '2026-03-12';
+const API_VERSION = '2026-04-20';
 const PLAN_NAME = 'TAKEFRAME SUBSCRIPTION';
 const PLAN_CONFIG = {
   annual: { cycle: 'P1Y', amount: 169000 },
